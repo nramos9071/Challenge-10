abefbe7660a7ece1be82b3fc4fe53cbb815b2d45
nramos9071 <nramos9071@gmail.com>
2024-10-23 18:00:36 -0500

    updated character colors

--- index.js
+++ index.js
@@ -23,9 +23,15 @@ const questions = [
     },
 
     {
+        type: 'input',
+        message: 'Please enter a color for the characters (keyword or hexadecimal).',
+        name: 'characterColor',
+    },
+
+    {
         type: 'type',
         message: 'Please enter a color keyword (OR a hexadecimal number).',
-        name: 'color',
+        name: 'shapeColor',
     },
 
     {
@@ -41,34 +47,36 @@ const questions = [
 
 ];
 
-function generateImg(shape, color) {
+function generateImg(shape, shapeColor, characters) {
     let svgElement;
 
     switch (shape) {
         case 'Circle':
-            svgElement = new Circle(300, 200, 50, 50, 40, color);
+            svgElement = new Circle(300, 200, 150, 100, 80, shapeColor);
             break;
         case 'Triangle':
-            svgElement = new Triangle(300, 200, "0,200", "100,0", "200,200", color);
+            svgElement = new Triangle(300, 200, "150,0", "0,200", "300,200", shapeColor);
             break;
         case 'Square':
-            svgElement = new Square(300, 200, 0, 0, 20, 20, color);
+            svgElement = new Square(300, 200, 50, 50, 20, 20, shapeColor);
             break;
     }
 
-    return svgElement.render();
-
+    return `
+        ${svgElement.render()}
+        <text x="50%" y="50%" fill="${characters.getColor()}" font-size="40" text-anchor="middle" dy=".3em">${characters.getCharacters()}</text>
+    `;
 }
 
 
 function init() {
     inquirer.prompt(questions).then(answers => {
-        const { name, shape, color, characters } = answers;
-        const characterObj = new Characters(name);
-        characterObj.addCharacters(characters);
-        const image = generateImg(shape, color);
+        const { name, shape, shapeColor, characters: charInput, characterColor } = answers;
+        const characterObj = new Characters(name, characterColor);
+        characterObj.addCharacters(charInput);
+        const image = generateImg(shape, shapeColor, characterObj);
         fs.writeFileSync('image.svg', image);
-        console.log(`SVG file has been created using ${shape}, ${color}, and characters: ${characterObj.getCharacters()}`);
+        console.log(`SVG file has been created using ${shape}, ${shapeColor} for shape and characters: ${characterObj.getCharacters()} with color ${characterObj.getColor()}`);
     });
 }
 
